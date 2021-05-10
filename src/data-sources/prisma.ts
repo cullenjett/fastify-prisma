c3a6a73f197f23df8c4a42b1ddf47e2711f1cb76
Cullen Jett <cullenjett@gmail.com>
2021-05-09 20:00:07 -0600

    prisma client connection management for local dev

--- src/data-sources/prisma.ts
+++ src/data-sources/prisma.ts
@@ -1,3 +1,24 @@
+/**
+ * Singleton instance of the PrismaClient.
+ * In development we add our `prisma` instance to the global object
+ * to prevent hot-reloading from creating multiple, unwanted instances
+ * of the PrismaClient.
+ *
+ * https://www.prisma.io/docs/guides/performance-and-optimization/connection-management
+ */
+
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+// Add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') {
+  global.prisma = prisma;
+}
