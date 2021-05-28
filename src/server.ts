a7821e08186fa9ae04d7ba9a6e45a1f0d1e22f73
Cullen Jett <cullenjett@gmail.com>
2021-05-28 17:57:05 -0600

    experimenting with adding a json schema using fluent-json-schema

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import pinoColada from 'pino-colada';
+import S from 'fluent-json-schema';
 
 import { prisma } from 'data-sources/prisma';
 
@@ -11,7 +12,37 @@ export function createServer() {
 
   const server = Fastify({ logger: loggerOptions });
 
-  server.get('/users', async () => {
+  const schema = {
+    response: {
+      200: S.array().items(
+        S.object()
+          .prop('id', S.integer())
+          .prop('email', S.string())
+          .prop('name', S.string())
+          .prop(
+            'posts',
+            S.array().items(
+              S.object()
+                .prop('id', S.integer())
+                .prop('createdAt', S.string())
+                .prop('updatedAt', S.string())
+                .prop('title', S.string())
+                .prop('content', S.oneOf([S.null(), S.string()]))
+                .prop('published', S.boolean())
+                .prop('authorId', S.integer())
+            )
+          )
+          .prop(
+            'profile',
+            S.object()
+              .prop('id', S.integer())
+              .prop('bio', S.string())
+              .prop('userId', S.integer())
+          )
+      ),
+    },
+  };
+  server.get('/users', { schema }, async (request) => {
     const users = await prisma.user.findMany({
       include: { posts: true, profile: true },
     });
